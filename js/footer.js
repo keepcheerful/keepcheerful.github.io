71b69465edec4551c5e830cf6f1ca274eea8300b
Git <2569934318@qq.com>
2023-07-28 15:00:04 +0800

    Site updated: 2023-07-28 15:00:04

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     document.querySelectorAll('.copyright')[0].innerHTML = '<div>©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 初心 </div>'
     show_date_time();
+    console.log(document.querySelectorAll("#container"));
     setTimeout(() => {
         document.querySelectorAll(".aplayer-icon")[8].addEventListener("click", function () {
             const aplayer = document.querySelectorAll('.aplayer')[0];
